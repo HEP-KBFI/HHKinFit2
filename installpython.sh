42ec3b6b45d138fb19c3c5971a4349a2a9aa7c97
Torben Lange <torben.lange@cern.ch>
2025-02-19 13:26:00 +0100

    make it run on lxplus

--- installpython.sh
+++ installpython.sh
@@ -1,6 +1,15 @@
-source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.24.08/x86_64-centos7-gcc48-opt/bin/thisroot.sh
+# update cmake when necessary i.e lxplus
+if [ "${HOSTNAME:0:6}" = lxplus ]; then
+    source /cvmfs/cms.cern.ch/el9_amd64_gcc12/external/cmake/3.28.3-6dd20ba899a48e46b39e41512ff0af64/etc/profile.d/init.sh
+fi
 mkdir -p build
 cd build
 make clean
-cmake .. -DPYTHON_LIBRARY_DIR="." -DPYTHON_EXECUTABLE="/usr/bin/python3"
+if [ "${HOSTNAME:0:6}" = lxplus ]; then
+    echo "lxplus"
+    echo "${USER}"
+    cmake .. -DPYTHON_LIBRARY_DIR="." -DPYTHON_EXECUTABLE="/usr/bin/python3" -DLXPLUS=ON -DBIND11LOC="/afs/cern.ch/user/t/${USER}/.local/lib/python3.9/site-packages/pybind11/share/cmake/pybind11/"
+else
+    cmake .. -DPYTHON_LIBRARY_DIR="." -DPYTHON_EXECUTABLE="/usr/bin/python3"
+fi
 make -j4
